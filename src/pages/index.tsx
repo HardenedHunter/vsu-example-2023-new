859854d56f0251cb6ba3a971591aaebb261e3305
HardenedHunter <specialforbattlenet@gmail.com>
2023-12-14 12:57:26 +0300

    Added different grid layouts for different resolutions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const Home: FC<HomeProps> = ({ products: initialProducts }) => {
       <section className="rounded-lg bg-slate-200 p-4">
         <Filters onFiltersChange={refetch} />
       </section>
-      <section className="mt-4 grid grid-cols-3 justify-between gap-10 rounded-lg bg-slate-200 p-4">
+      <section className="mt-4 grid grid-cols-1 justify-between gap-10 rounded-lg bg-slate-200 p-4 sm:grid-cols-2 lg:grid-cols-3">
         {!isLoading &&
           products.map((p) => <ProductCard key={p.id} product={p} />)}
       </section>
